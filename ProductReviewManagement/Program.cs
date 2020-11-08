28fe10b02277dc22fb53ad6e9e7affad94e74996
Vishal Garg <vishugarg10004@gmail.com>
2020-11-08 10:25:10 +0530

    [Vishal] Welcome message and linq to Datatable

---
+++ ProductReviewManagement/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ProductReviewManagement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Product Review Management");
+            LinqToDataTable linqToDataTable = new LinqToDataTable();
+            linqToDataTable.AddDataTable();
+        }
+    }
+}
