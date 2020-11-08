5c7047f58fa1a64a06476301179a614b68613d5c
Vishal Garg <vishugarg10004@gmail.com>
2020-11-08 11:57:20 +0530

    [Vishal] created list of product review management

---
+++ ProductReviewManagement/ProductManagement.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Data;
+
+namespace ProductReviewManagement
+{
+    class ProductManagement
+    {
+        
+    }
+}
